83af9d0d2ee0d1f65767cc67800209bb8d2f694a
Avijit6152 <avijitkumarlenka1997@outlook.com>
2024-03-02 21:49:41 +0530

    modify code

--- AC/first.js
+++ AC/first.js
@@ -1,1 +1,187 @@
 console.log("Avijit Kumar Lenka");
+
+
+let a=10;
+let b=50;
+
+console.log(a+b);
+
+
+let fname;
+let mname;
+let lname;
+let age;
+let gender;
+let email;
+let mobile;
+let address;
+
+let  personalDetails=
+{
+
+     fname:"Avijit",
+     mname:"Kumar",
+     lname:"Lenka",
+     age:27,
+     gender:"male",
+     email:"ävijitkumarlenka@gmail.com",
+     mobile:8249232268,
+     address:"Cuttack,Odisha,753001"
+
+
+}
+
+console.log(personalDetails);
+
+let familyInfo=[
+    {
+
+        fname:"Avijit",
+        mname:"Kumar",
+        lname:"Lenka",
+        age:27,
+        gender:"male",
+        email:"ävijitkumarlenka@gmail.com",
+        mobile:8249232268,
+        address:"Cuttack,Odisha,753001"
+   
+   
+   },
+
+   {
+
+    fname:"Avijit1",
+    mname:"Kumar",
+    lname:"Lenka",
+    age:27,
+    gender:"male",
+    email:"ävijitkumarlenka@gmail.com",
+    mobile:8249232268,
+    address:"Cuttack,Odisha,753001"
+
+
+},
+
+{
+
+    fname:"Avijit11",
+    mname:"Kumar",
+    lname:"Lenka",
+    age:27,
+    gender:"male",
+    email:"ävijitkumarlenka@gmail.com",
+    mobile:8249232268,
+    address:"Cuttack,Odisha,753001"
+
+
+},
+
+{
+
+    fname:"Avijit111",
+    mname:"Kumar",
+    lname:"Lenka",
+    age:27,
+    gender:"male",
+    email:"ävijitkumarlenka@gmail.com",
+    mobile:8249232268,
+    address:"Cuttack,Odisha,753001"
+
+
+},
+
+{
+
+    fname:"Avijit1111",
+    mname:"Kumar",
+    lname:"Lenka",
+    age:27,
+    gender:"male",
+    email:"ävijitkumarlenka@gmail.com",
+    mobile:8249232268,
+    address:"Cuttack,Odisha,753001"
+
+
+}
+]
+
+console.log(familyInfo);
+
+console.log(familyInfo[0].fname);
+console.log(familyInfo[1].fname);
+console.log(familyInfo[2].fname);
+console.log(familyInfo[3].fname);
+console.log(familyInfo[4].fname);
+
+// Retrive
+
+console.log(personalDetails.fname);
+
+
+
+
+let name;
+let  quantity;
+let pricePerKg;
+let tPrice;
+let discount;
+let fruits=[
+    {
+        name:"mango",
+        quantity:50,
+        pricePerKg:100,
+        // tPrice:quantity * pricePerKg,
+        discount:5
+    },
+    {
+        name:"apple",
+        quantity:40,
+        pricePerKg:130,
+        // tPrice:quantity * pricePerKg,
+        discount:5
+    },
+    {
+        name:"orange",
+        quantity:30,
+        pricePerKg:90,
+        // tPrice:quantity * pricePerKg,
+        discount:5
+    },
+    {
+        name:"grape",
+        quantity:60,
+        pricePerKg:125,
+        // tPrice:quantity * pricePerKg,
+        discount:5
+    },
+
+    {
+        name:"guava",
+        quantity:35,
+        pricePerKg:115,
+        // tPrice:quantity * pricePerKg,
+        discount:5
+    },
+    {
+        name:"watermelon",
+        quantity:100,
+        pricePerKg:25,
+        // tPrice:quantity * pricePerKg,
+        discount:5
+    }
+]
+
+console.log(fruits);
+
+// let p=console.log(fruits[0].pricePerKg);
+// let q=console.log(fruits[0].quantity);
+// let tp= p * q ;
+// console.log(tp);
+
+
+
+// console.log(fruits[1].tPrice);
+// console.log(fruits[2].tPrice);
+// console.log(fruits[3].tPrice);
+// console.log(fruits[4].tPrice);
+// console.log(fruits[5].tPrice);
