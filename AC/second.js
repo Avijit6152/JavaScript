19528e30a842276ee0b4da59caa7808238b61c26
Avijit6152 <avijitkumarlenka1997@outlook.com>
2024-03-03 10:06:05 +0530

    added new files

---
+++ AC/second.js
@@ -0,0 +1,22 @@
+let a=65;
+let b=77;
+let c=33.33;
+let d=-45;
+let e=-66.987;
+
+console.log("a = "+a," b = "+b," c = "+c," d = "+d," e = "+e);
+let result =a+b+c+d+e;
+let result2 = a-b-c-d-e;
+let result3 = a*b*c*d*e;
+let result4 = a/b/c/d/e;
+console.log("Total Sum of Numbers = "+result);
+console.log("Total Sub of Numbers = "+result2);
+console.log("Total mul of Numbers = "+result3);
+console.log("Total div of Numbers = "+result4);
+
+console.log("Total rem of Results = "+ result3 % result);
+
+let m=115;
+let n=10;
+
+console.log("Total rem of Results = "+ m % n);
